c1cb1198dd2849312a536c7a5e3fe6d050eaf278
BEATiFY Group <80850311+BEATiFYAUDIO@users.noreply.github.com>
2025-08-15 15:52:14 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Pediatric-Anesthesia-Calculator/', // <-- repo name
+  base: '/Pediatric-Anesthesia-Calculator/', // exactly your repo name
 })
